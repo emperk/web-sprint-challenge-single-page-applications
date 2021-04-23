57f5549e6697d7a9cad099d833a1cdf5ea1ddde6
Emily Perkins <emilyjperkinss@Emilys-MacBook-Pro.local>
2021-04-23 14:56:38 -0700

    Getting home page set up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
-import React from "react";
+// import React from "react";
 
-const App = () => {
+// const App = () => {
+//   return (
+//     <>
+//       <h1>Lambda Eats</h1>
+//       <p>You can remove this code and create your own header</p>
+//     </>
+//   );
+// };
+// export default App;
+
+import React, { useState, useEffect } from 'react';
+import { Route, Link, Switch } from 'react-router-dom';
+
+// import components for different routes
+
+// import data
+
+function fetchStock() {
+  return Promise.resolve({ success: true, data })
+}
+
+export default function App(props) {
+  const [stock, setStock] = useState([])
+
+  useEffect(() => {
+    fetchStock().then(res => setStock(res.data))
+  }, [])
+
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
-  );
-};
-export default App;
+    <div className='App'>
+      <nav>
+        <h1>Lambda Eats</h1>
+        <div className='nav-links'>
+          /* LINKS GO HERE */
+        </div>
+      </nav>
+
+      <Switch>
+        /* ROUTES GO HERE */
+      </Switch>
+    </div>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import App from "./App";
+// // import { render } from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// ReactDOM.render(<App />, document.getElementById("root"));
+
+import React from "react";
+import { render } from "react-dom";
+
+import App from "./App";
+import "./index.css";
+import { BrowserRouter as Router } from 'react-router-dom';
+
+render(
+  <Router>
+    <App />
+  </Router>
+  , document.querySelector('#root')
+)
